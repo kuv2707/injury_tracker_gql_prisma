936eca1f0b75aff31748f661cdeee0e8c34444db
Kislay <88523649+kuv2707@users.noreply.github.com>
2023-10-20 18:19:44 +0530

    [ADDED] scoping to injury reports based on user

--- index.ts
+++ index.ts
@@ -3,45 +3,58 @@ import { graphqlHTTP } from "express-graphql";
 const app = express();
 
 import { schema } from "./schemaAndHandlers";
-import { auth } from 'express-openid-connect';
-import { requiresAuth } from 'express-openid-connect';
+import { auth } from "express-openid-connect";
+import { requiresAuth } from "express-openid-connect";
+import prisma from "./prisma";
+import { BSON } from "bson";
+import { json } from "stream/consumers";
 
 const config = {
 	authRequired: false,
 	auth0Logout: true,
-	secret: 'fhgkjregehgjkfbnfgbjkgnmegfkjbfb2441nlgmgfnj',
-	baseURL: 'http://localhost:3000',
-	clientID: 'LmFvboQZxZAcKydKFOdVlo7wfv06rLTx',
-	issuerBaseURL: 'https://dev-xfulotfa0wxqhmv8.us.auth0.com'
+	secret: "fhgkjregehgjkfbnfgbjkgnmegfkjbfb2441nlgmgfnj",
+	baseURL: "http://localhost:3000",
+	clientID: "LmFvboQZxZAcKydKFOdVlo7wfv06rLTx",
+	issuerBaseURL: "https://dev-xfulotfa0wxqhmv8.us.auth0.com",
 };
 
 app.use(auth(config));
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: express.Response) => {
-	res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+app.get("/", async (req: Request, res: express.Response) => {
+	if (req.oidc.isAuthenticated() && req.oidc.user) {
+		console.log("creating");
+		if((await prisma.user.findFirst({where:{unique_id:req.oidc.user.sub}}))===null)
+		{
+			await prisma.user.create({
+				data: {
+					id: new BSON.ObjectId().toString(),
+					unique_id: req.oidc.user.sub,
+					name: req.oidc.user.name,
+					email: req.oidc.user.email,
+					picture_url: req.oidc.user.picture_url,
+					nickname: req.oidc.user.nickname,
+				},
+			});
+
+		}
+	}
+	res.send(
+		(req.oidc.isAuthenticated() ? "Logged in" : "Logged out") +
+			JSON.stringify(req.oidc.user)
+	);
 });
 
-interface RequestWithBody extends express.Request {
-	body: { [key: string]: string | undefined };
-	query: { [key: string]: string | undefined };
-}
+app.use((req: any, _, next: express.NextFunction) => {
+	// console.log(req.query);
+	req.user = req.oidc.user;
+	req.user.unique_id = req.oidc.user.sub;
+	next();
+});
 
-app.use(
-	(
-		req: RequestWithBody,
-		res: express.Response,
-		next: express.NextFunction
-	) => {
-		// console.log(req.query);
-		next();
-	}
-);
-
-app.get('/profile', requiresAuth(), (req, res) => {
+app.get("/profile", requiresAuth(), (req, res) => {
 	res.send(JSON.stringify(req.oidc.user));
 });
 
@@ -57,7 +70,6 @@ app.use(
 		return options;
 	})
 );
-
 
 app.listen(3000, () => {
 	console.log("Server running on http://localhost:3000");
--- schemaAndHandlers.ts
+++ schemaAndHandlers.ts
@@ -12,13 +12,6 @@ import {
 
 import prisma from "./prisma";
 import { BSON } from "bson";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import { OAuth2Client } from 'google-auth-library';
-// import { config } from "dotenv";
-// config();
-
-const jwt_secret: string = "asdfghjkl;asdfghjkl;"
 
 const RegionType = new GraphQLObjectType({
 	name: "Region",
@@ -86,16 +79,14 @@ const UserType = new GraphQLObjectType({
 			type: GraphQLNonNull(GraphQLString),
 			resolve: (user) => user.email,
 		},
-	}),
-});
-
-
-const AuthResponseType = new GraphQLObjectType({
-	name: "AuthResponse",
-	description: "Represents an authentication response",
-	fields: () => ({
-		user: { type: UserType },
-		token: { type: GraphQLString },
+		picture_url: {
+			type: GraphQLNonNull(GraphQLString),
+			resolve: (user) => user.picture_url,
+		},
+		nickname: {
+			type: GraphQLNonNull(GraphQLString),
+			resolve: (user) => user.nickname,
+		}
 	}),
 });
 
@@ -116,7 +107,7 @@ const rootQueryType = new GraphQLObjectType({
 						where: {
 							id: args.id,
 							//todo:
-							// userId:context.user.id
+							userId:context.user.unique_id
 						},
 					});
 				},
@@ -125,11 +116,12 @@ const rootQueryType = new GraphQLObjectType({
 				type: new GraphQLList(InjuryType),
 				description: "List of injuries reported",
 				resolve: async (parent, args, context) => {
+					console.log(context.user)
 					return await prisma.injuryReport.findMany({
 						//todo:
-						// where:{
-						// 	userId:context.user.id
-						// }
+						where:{
+							userId:context.user.unique_id
+						}
 					});
 				},
 			},
@@ -139,12 +131,12 @@ const rootQueryType = new GraphQLObjectType({
 				args: {
 					id: { type: GraphQLNonNull(GraphQLString) },
 				},
-				resolve: async (parent, args) => {
-					console.log(args.id);
+				resolve: async (parent, args,context) => {
+					console.log(args.id,context.user);
 					return await prisma.injuryReport.delete({
 						where: {
 							id: args.id,
-							//todo: userId: context.user.id
+							userId: context.user.unique_id
 						},
 					});
 				},
@@ -171,17 +163,16 @@ const rootQueryType = new GraphQLObjectType({
 					return await prisma.injuryReport.update({
 						where: {
 							id: args.id,
-							//todo: userId: context.user.id,
+							userId: context.user.unique_id,
 						},
 						data,
 					});
 				},
 			},
-			me: {
+			profile: {
 				type: UserType,
 				resolve: (parent, args, context) => {
-					// Implement authentication logic here (e.g., check for JWT token).
-					// Return the authenticated user or null if not authenticated.
+					return context.user;
 				},
 			},
 		};
@@ -206,13 +197,6 @@ const RegionInputType = new GraphQLInputObjectType({
 	},
 });
 
-const ErrorResponseType = new GraphQLObjectType({
-	name: "ErrorResponse",
-	description: "Represents an error response",
-	fields: () => ({
-		error: { type: GraphQLString },
-	}),
-});
 
 //create injury
 const rootMutationType = new GraphQLObjectType({
@@ -230,121 +214,20 @@ const rootMutationType = new GraphQLObjectType({
 				},
 			},
 			resolve: async (parent, args, context) => {
+				console.log(context.user)
 				const newinj = await prisma.injuryReport.create({
 					data: {
+
 						name_reporter: args.name_reporter,
 						body_map: args.body_map,
 						id: new BSON.ObjectId().toString(),
-						userId: new BSON.ObjectId().toString(),//todo:  =context.user.id
+						userId: context.user.unique_id,
 					},
 				});
 				return newinj;
 			},
 		},
-		signup: {
-			type: AuthResponseType,
-			args: {
-				name: { type: GraphQLNonNull(GraphQLString) },
-				email: { type: GraphQLNonNull(GraphQLString) },
-				password: { type: GraphQLNonNull(GraphQLString) },
-			},
-			resolve: async (_, args, context) => {
-				let user = await prisma.user.findFirst({
-					where: {
-						email: args.email
-					}
-				});
-				if (user) {
-					throw new GraphQLError("User already registered");
-				}
-				const token = jwt.sign({ email: args.email }, jwt_secret);
-				const hashedPassword = await bcrypt.hash(args.password, 10);
-				user = await prisma.user.create({
-					data: {
-						name: args.name,
-						email: args.email,
-						password: hashedPassword
-					}
-				})
-				context.user = user;
-				return {
-					token,
-					user
-				}
-			},
-		},
-		login: {
-			type: AuthResponseType,
-			args: {
-				email: { type: GraphQLNonNull(GraphQLString) },
-				password: { type: GraphQLNonNull(GraphQLString) },
-			},
-			resolve: async (_, args, context) => {
-				const user = await prisma.user.findFirst({
-					where: {
-						email: args.email
-					}
-				})
-				if (!user) {
-					throw new GraphQLError("User doesn't exist");
-				}
-				const isMatch = await bcrypt.compare(args.password, user.password);
-				if (!isMatch) throw new GraphQLError("Check Credentials!");
-				const token = jwt.sign({ email: args.email }, jwt_secret);
-				return {
-					token,
-					user
-				}
-			},
-		},
-		googleOAuth: {
-			type: AuthResponseType,
-			args: {
-				googleAccessToken: { type: GraphQLNonNull(GraphQLString) },
-			},
-			resolve: async (parent, args, context) => {
-				const { googleAccessToken } = args;
-
-				const googleClientId = 'LmFvboQZxZAcKydKFOdVlo7wfv06rLTx';
-				const client = new OAuth2Client(googleClientId);
-
-				try {
-					const ticket = await client.verifyIdToken({
-						idToken: googleAccessToken,
-						audience: googleClientId,
-					});
-
-					const payload: any = ticket.getPayload();
-
-					let user = await prisma.user.findFirst({
-						where: {
-							email: payload.email,
-						},
-					});
-
-					// if (!user) {
-					// 	const newUser = await prisma.user.create({
-					// 		data: {
-					// 			name: payload.name,
-					// 			email: payload.email,
-					// 		},
-					// 	});
-					// 	// Associate newUser with the user variable
-					// 	user = newUser;
-					// }
-
-					// Generate an authentication token
-					const token = jwt.sign({ email: payload.email }, jwt_secret);
-
-					return {
-						token,
-						user,
-					};
-				} catch (error: any) {
-					throw new Error(error.message);
-				}
-			},
-		},
+		
 	})
 },
 );
